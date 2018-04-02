abed64fd7d8f82e277275f9833be107071b8eddb
u_programmer2 programmer2 <u_programmer2@programmer2.METEO>
2018-04-02 16:55:04 +0300

    fix

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,14 +10,14 @@ class UsersController extends Controller
     /**
      * @param Request $request
      * @param $userId
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
      */
     public function edit(Request $request, $userId)
     {
         if ($request->method() === 'GET') {
             return view('admin.user_edit');
         } else if ($request->method() === 'POST') {
-            var_dump($request->method());
+            return redirect('admin.users');
         }
     }
 
